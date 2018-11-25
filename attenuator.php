348f3370a19f883fd1208e467ad9c57fdb3a0e0a
Pedro F. Toledo <pedrotoledocorrea@gmail.com>
2018-11-25 00:42:56 +0000

    Moving attenuator communication to python

--- attenuator.php
+++ attenuator.php
@@ -86,7 +86,7 @@
   
   function execute_command(&$log, $command, $label){
       $log = $log."\nReceived:\n$label\n\n";
-      exec("sudo -u pi /var/www/html/runAttenuator \"$command\" 2>&1", $result);
+      exec("sudo -u pi /var/www/html/runAttenuator.py \"$command\" 2>&1", $result);
       $log = $log."Result:\n";
       for ($i=0; $i<count($result); $i++){
         if (strlen($result[$i]) != 0) {
